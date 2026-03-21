3244672f3cc81529555092802b296b4c95f1259e
yuyuoookkk <junior10marius@gmail.com>
2026-03-21 21:27:28 +0800

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-2   import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
